6fcf1fa4eb0c605da7c6a6c382f58a37de3e37c6
Gabriel Costa <119276145+eubielzin@users.noreply.github.com>
2024-04-05 22:06:23 -0300

    Create DesafioMedia.js

---
+++ DesafioMedia.js
@@ -0,0 +1,8 @@
+let nota1 = 8;
+let nota2 = 9;
+let nota3 = 7;
+
+function calcularMedia(nota1, nota2, nota3){
+  console.log(`A média da aluna é ${(nota1 + nota2 + nota3)/3}`)
+}
+calcularMedia(nota1, nota2, nota3)
