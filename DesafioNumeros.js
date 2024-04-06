0e68705c19f0171166124b3035639634d1bcb7ae
Gabriel Costa <119276145+eubielzin@users.noreply.github.com>
2024-04-05 22:08:10 -0300

    Create DesafioNumeros.js

---
+++ DesafioNumeros.js
@@ -0,0 +1,6 @@
+let listaNumeros = [15,8,90,75,102,6,2];
+
+let maior = Math.max(...listaNumeros)
+let menor = Math.min(...listaNumeros)
+
+console.log(`O numero maior digitado é ${maior} e o menor  é ${menor}`)
